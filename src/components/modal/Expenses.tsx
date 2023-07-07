044338d6834dcc0482fb91a0b1aeb5403d9d29d1
BearHumanS <bye0651@naver.com>
2023-07-07 21:10:04 +0900

    docs/api폴더 하위 파일 확장자 수정 및 calendar 변수명 변

--- src/components/modal/Expenses.tsx
+++ src/components/modal/Expenses.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { createdExpense, category } from '@/lib/api/Api'
 import styled from 'styled-components'
 
 function Expenses() {
