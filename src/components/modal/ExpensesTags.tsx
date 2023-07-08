8fe07f50efefdd64dc476135bf8907013fd844fb
BearHumanS <bye0651@naver.com>
2023-07-08 19:34:30 +0900

    feat/ 금액 입력 및 소비태그 작성ing

--- src/components/modal/ExpensesTags.tsx
+++ src/components/modal/ExpensesTags.tsx
@@ -1,75 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function Expenses() {
   const [dropDown, setDropDown] = useState(true);
-  const [selectTag, setSelectTag] = useState('소비태그')
+  const [selectTag, setSelectTag] = useState('소비태그');
 
   const toggleDropDown = () => {
     setDropDown(!dropDown);
   };
 
   const expensesTags = [
-    {id: "식비", value: "식비"},
-    {id: "교통비", value: "교통비"},
-    {id: "문화생활", value: "문화생활"},
-    {id: "생필품", value: "생필품"},
-    {id: "의류", value: "의류"},
-    {id: "미용", value: "미용"},
-    {id: "의료/건강", value: "의료/건강"},
-    {id: "교육", value: "교육"},
-    {id: "통신비", value: "통신비"},
-    {id: "회식", value: "회식"},
-    {id: "경조사", value: "경조사"},
-    {id: "저축", value: "저축"},
-    {id: "가전", value: "가전"},
-    {id: "공과금", value: "공과금"},
-    {id: "카드대금", value: "카드대금"},
-    {id: "기타", value: "기타"}
-  ]
+    { id: '식비', value: '식비' },
+    { id: '교통비', value: '교통비' },
+    { id: '문화생활', value: '문화생활' },
+    { id: '생필품', value: '생필품' },
+    { id: '의류', value: '의류' },
+    { id: '미용', value: '미용' },
+    { id: '의료/건강', value: '의료/건강' },
+    { id: '교육', value: '교육' },
+    { id: '통신비', value: '통신비' },
+    { id: '회식', value: '회식' },
+    { id: '경조사', value: '경조사' },
+    { id: '저축', value: '저축' },
+    { id: '가전', value: '가전' },
+    { id: '공과금', value: '공과금' },
+    { id: '카드대금', value: '카드대금' },
+    { id: '기타', value: '기타' },
+  ];
 
   const selectTagHandle = (tagValue: string) => {
     setSelectTag(tagValue);
     setDropDown(!dropDown);
-  }
+  };
 
   return (
     <DropdownWrapper onClick={toggleDropDown}>
       <Title>{selectTag}</Title>
       <Menu closed={dropDown}>
         <ItemBoard>
-          {expensesTags.map(expense => (
-          <MenuItem key={expense.id} onClick={() => selectTagHandle(expense.value)}>{expense.value}</MenuItem>
-        ))}
+          {expensesTags.map((expense) => (
+            <MenuItem
+              key={expense.id}
+              onClick={() => selectTagHandle(expense.value)}
+            >
+              {expense.value}
+            </MenuItem>
+          ))}
         </ItemBoard>
       </Menu>
     </DropdownWrapper>
-  )
+  );
 }
 
 const DropdownWrapper = styled.div`
-    transition: all 0.2s ease-in-out;
-    overflow: hidden;
-    margin: 30px;
-    background: rgb(248, 248, 248);
-    border: solid 1px rgb(222, 222, 222);
-    width: 320px;
-    cursor: pointer;
-    text-align: center;
+  transition: all 0.2s ease-in-out;
+  overflow: hidden;
+  margin: 30px;
+  background: rgb(248, 248, 248);
+  border: solid 1px rgb(222, 222, 222);
+  width: 320px;
+  cursor: pointer;
+  text-align: center;
 `;
 
-const Title = styled.h2 `
+const Title = styled.h2`
   height: 48px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Menu = styled.div<{ closed: boolean }>`
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-    height: ${props => (props.closed ? '0px' : 'auto')};
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  height: ${(props) => (props.closed ? '0px' : 'auto')};
 `;
 
 const ItemBoard = styled.div`
@@ -78,17 +83,16 @@ const ItemBoard = styled.div`
 }
 `;
 
-
 const MenuItem = styled.span`
-    font-size: 16px;
-    text-align: center;
-    padding: 14px 10px;
-    flex: 1 0 24%;
-    margin: 0.5%;
+  font-size: 16px;
+  text-align: center;
+  padding: 14px 10px;
+  flex: 1 0 24%;
+  margin: 0.5%;
 
-    &:hover {
-      color: red;
-}
+  &:hover {
+    color: red;
+  }
 `;
 
-export default Expenses
+export default Expenses;
