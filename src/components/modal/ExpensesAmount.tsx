561d85e4e52c2552646a4154d9bbbe82d367e5d7
BearHumanS <bye0651@naver.com>
2023-07-08 19:34:30 +0900

    feat/ 금액 입력 및 소비태그 작성ing

---
+++ src/components/modal/ExpensesAmount.tsx
@@ -0,0 +1,49 @@
+import React, { ChangeEvent, useState, useRef, useEffect } from 'react'
+import styled from 'styled-components'
+
+function ExpensesAmount() {
+    const [amount, setAmount] = useState(0);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, []);
+
+    const handleChage = (e: ChangeEvent<HTMLInputElement>) => {
+        const input = e.target.value;
+        const numericInput = Number(input.replace(/[^0-9]/g, ''));
+        setAmount(numericInput);
+    }
+
+   
+  return (
+    <AmountContainer>
+        <Won>\</Won><AmountInput ref={inputRef} dir="rtl" type="text" value={amount.toLocaleString()} onChange={handleChage}/>
+    </AmountContainer>
+  )
+}
+
+const AmountContainer = styled.div `
+    width: 150px;
+    border-bottom: 1px solid;
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+`
+
+const Won = styled.span `
+`
+
+const AmountInput = styled.input `
+    border: none;
+    width: 100px;
+
+    &:focus {
+        border: none;
+        outline: none;
+    }
+`
+
+export default ExpensesAmount
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import Expenses from './Expenses'
+import ExpensesTags from './ExpensesTags'
+import ExpensesAmount from './ExpensesAmount'
+import { styled } from 'styled-components'
 
 function Modal() {
   return (
-    <Expenses />
+    <Container>
+      <ExpensesAmount />
+      <ExpensesTags />
+    </Container>
   )
 }
+
+const Container = styled.div `
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 export default Modal
