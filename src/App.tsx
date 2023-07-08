2b211adcbc3e22b3ab75f813ff0d76582bb96d44
BearHumanS <bye0651@naver.com>
2023-07-08 19:34:30 +0900

    feat/ 금액 입력 및 소비태그 작성ing

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Header from '@/components/common/Header';
 import NotFound from '@/components/common/NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/components/Home/Home';
+import Modal from '@/components/modal/Modal';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Modal />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />}></Route>
