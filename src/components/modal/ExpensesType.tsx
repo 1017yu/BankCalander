16632829f41d26e68bdee38ccf4b3c41306ed430
BearHumanS <bye0651@naver.com>
2023-07-09 16:42:54 +0900

    수정 중

---
+++ src/components/modal/ExpensesType.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import styled from 'styled-components';
+
+function ExpensesType() {
+    const [selectType, setSelectType] = useState('')
+  return (
+    <div>ExpensesType</div>
+  )
+}
+
+export default ExpensesType
