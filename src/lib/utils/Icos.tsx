87f7f81dca867dadbb8330bbc70d6c7ee102df91
BearHumanS <bye0651@naver.com>
2023-07-10 21:54:42 +0900

    docs/common icons 정리

---
+++ src/lib/utils/Icos.tsx
@@ -0,0 +1,54 @@
+import {
+    FaUtensils,
+    FaBus,
+    FaFilm,
+    FaSprayCan,
+    FaTshirt,
+    FaCut,
+    FaStethoscope,
+    FaPencilAlt,
+    FaMobileAlt,
+    FaBeer,
+    FaRegHandshake,
+    FaPiggyBank,
+    FaTv,
+    FaReceipt,
+    FaCreditCard,
+    FaEllipsisH
+  } from 'react-icons/fa';
+  
+  const foodIcon = <FaUtensils />;
+  const transportationIcon = <FaBus />;
+  const culturalIcon = <FaFilm />;
+  const dailyNecessityIcon = <FaSprayCan />;
+  const clothesIcon = <FaTshirt />;
+  const beautyIcon = <FaCut />;
+  const medicalHealthIcon = <FaStethoscope />;
+  const educationIcon = <FaPencilAlt />;
+  const communicationIcon = <FaMobileAlt />;
+  const gatheringIcon = <FaBeer />;
+  const eventsIcon = <FaRegHandshake />;
+  const savingIcon = <FaPiggyBank />;
+  const electronicsIcon = <FaTv />;
+  const utilitiesIcon = <FaReceipt />;
+  const cardIcon = <FaCreditCard />;
+  const etcIcon = <FaEllipsisH />;
+  
+  export {
+    foodIcon,
+    transportationIcon,
+    culturalIcon,
+    dailyNecessityIcon,
+    clothesIcon,
+    beautyIcon,
+    medicalHealthIcon,
+    educationIcon,
+    communicationIcon,
+    gatheringIcon,
+    eventsIcon,
+    savingIcon,
+    electronicsIcon,
+    utilitiesIcon,
+    cardIcon,
+    etcIcon
+  }
