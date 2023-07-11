cd329164c84550c709958f37767f57d92e166755
BearHumanS <bye0651@naver.com>
2023-07-11 14:19:19 +0900

    style/스타일 추가

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -14,10 +14,12 @@ function Modal() {
   const [amount, setAmount] = useState(0);
   const [tag, setTag] = useState('');
   const [paymentMethod, setPaymentMethod] = useState('');
+  const [activeButton, setActiveButton] = useState('');
 
   const handleButtonClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const formType = e.currentTarget.dataset.formType;
     setType(formType);
+    setActiveButton(formType || '');
   };
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,8 +36,12 @@ function Modal() {
   };
 
   const handleSubmit =  async () => {
+    let formAmount = amount;
+    if (type === 'expense') {
+      formAmount = -amount;
+    }
    const data = {
-    amount,
+    amount: formAmount,
     category: tag,
     date: new Date().toISOString()
     };
@@ -45,8 +51,8 @@ function Modal() {
   return (
     <Container>
       <ButtonCotainer>
-        <DepositButton $green="true" data-form-type="deposit" onClick={handleButtonClick}>입금</DepositButton>
-        <ExpenseButton $red="ture" data-form-type="expense" onClick={handleButtonClick}>지출</ExpenseButton>
+        <DepositButton $green="true" data-form-type="deposit" $active={activeButton === 'deposit'} onClick={handleButtonClick}>입금</DepositButton>
+        <ExpenseButton $red="ture" data-form-type="expense" $active={activeButton === 'expense'} onClick={handleButtonClick}>지출</ExpenseButton>
       </ButtonCotainer>
       <ExpensesAmount amount={amount} handleAmountChange={handleAmountChange}/>
       {type === 'deposit' ? <DepositTag handleTagChange={handleTagChange} /> : 
@@ -65,20 +71,19 @@ const Container = styled.div `
   justify-content: center;
   align-items: center;
   margin: 0 auto;
-`
+`;
 
 const ButtonCotainer = styled.div `
   display: flex;
   gap: 50px;
   justify-content: center;
   margin: 10px;
+`;
 
-  
-`
-
-const DepositButton = styled.button  <{
-  $green?: string;
-}> `
+const DepositButton = styled.button<{ 
+  $green?: string 
+  $active?: string
+}>`
   width: 100px;
   height: 30px;
   border-radius: 8px;
@@ -86,18 +91,31 @@ const DepositButton = styled.button  <{
   font-weight: bold;
   border: none;
   cursor: pointer;
+  transition: all ease 1s 0s;
 
   ${(props) =>
     props.$green &&
     css`
-      background-color: ${theme.colors.green};
-      color: #ffffffdb;
-      background-image: linear-gradient(315deg, #b9fad9, transparent);
+      &:hover {
+        background-color: ${theme.colors.green};
+        color: #ffffffdb;
+        background-image: linear-gradient(315deg, #b9fad9, transparent);
+      }
     `}
-`
+
+    ${(props) =>
+      props.$active &&
+      css`
+        background-color: ${theme.colors.green};
+        color: #ffffffdb;
+        background-image: linear-gradient(315deg, #b9fad9, transparent);
+      `}
+`;
+
 
 const ExpenseButton = styled.button <{
   $red?: string;
+  $active?: string
 }> `
   width: 100px;
   height: 30px;
@@ -106,21 +124,40 @@ const ExpenseButton = styled.button <{
   font-weight: bold;
   border: none;
   cursor: pointer;
+  transition: all ease 1s 0s;
 
   ${(props) =>
     props.$red &&
     css`
+     &: hover {
       background-color: ${theme.colors.red};
       color: #ffffffdb;
       background-image: linear-gradient(315deg, #e6b0c3, transparent);
+      }
     `}
-`
+
+    ${(props) =>
+      props.$active &&
+      css`
+        background-color: ${theme.colors.red};
+        color: #ffffffdb;
+        background-image: linear-gradient(315deg, #e6b0c3, transparent);
+      `}
+`;
 
 const Submit = styled.button `
   width: 250px;
-  height: 30px;
+  height: 50px;
   margin: 10px;
+  cursor: pointer;
+  border-radius: 8px;
+  border: none;
+  font-size: 18px;
+  font-weight: bold;
 
- 
-`
+  &: hover {
+    background-color: #33ff99;
+    color: #ffffffdb;
+  }
+`;
 export default Modal
