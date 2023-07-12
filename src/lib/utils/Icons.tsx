777e75cc7dda1a439de445766b7ebadb707249aa
Kled <1017yu@daum.net>
2023-07-12 19:48:53 +0900

    fix: conflict 수정

--- src/lib/utils/Icons.tsx
+++ src/lib/utils/Icons.tsx
@@ -1,4 +1,54 @@
 import {
+  FaUtensils,
+  FaBus,
+  FaFilm,
+  FaSprayCan,
+  FaTshirt,
+  FaCut,
+  FaStethoscope,
+  FaPencilAlt,
+  FaMobileAlt,
+  FaBeer,
+  FaRegHandshake,
+  FaPiggyBank,
+  FaTv,
+  FaReceipt,
+  FaCreditCard,
+  FaEllipsisH,
+  FaMoneyBill,
+  FaMoneyCheck,
+  FaMoneyCheckAlt,
+} from 'react-icons/fa';
+
+const foodIcon = <FaUtensils />;
+const transportationIcon = <FaBus />;
+const culturalIcon = <FaFilm />;
+const dailyNecessityIcon = <FaSprayCan />;
+const clothesIcon = <FaTshirt />;
+const beautyIcon = <FaCut />;
+const medicalHealthIcon = <FaStethoscope />;
+const educationIcon = <FaPencilAlt />;
+const communicationIcon = <FaMobileAlt />;
+const gatheringIcon = <FaBeer />;
+const eventsIcon = <FaRegHandshake />;
+const savingIcon = <FaPiggyBank />;
+const electronicsIcon = <FaTv />;
+const utilitiesIcon = <FaReceipt />;
+const cardIcon = <FaCreditCard />;
+const etcIcon = <FaEllipsisH />;
+const salaryIcon = <FaMoneyBill />;
+const moneyIcon = <FaMoneyCheck />;
+const cashIcon = <FaMoneyCheckAlt />;
+const debitCardIcon = <FaCreditCard />;
+const creditCardIcon = <FaCreditCard />;
+
+export {
+  foodIcon,
+  transportationIcon,
+  culturalIcon,
+  dailyNecessityIcon,
+  clothesIcon,
+  beautyIcon,
   medicalHealthIcon,
   educationIcon,
   communicationIcon,
@@ -15,70 +65,3 @@ import {
   debitCardIcon,
   creditCardIcon,
 };
-=======
-    FaUtensils,
-    FaBus,
-    FaFilm,
-    FaSprayCan,
-    FaTshirt,
-    FaCut,
-    FaStethoscope,
-    FaPencilAlt,
-    FaMobileAlt,
-    FaBeer,
-    FaRegHandshake,
-    FaPiggyBank,
-    FaTv,
-    FaReceipt,
-    FaCreditCard,
-    FaEllipsisH,
-    FaMoneyBill,
-    FaMoneyCheck,
-    FaMoneyCheckAlt,
-  } from 'react-icons/fa';
-  
-  const foodIcon = <FaUtensils />;
-  const transportationIcon = <FaBus />;
-  const culturalIcon = <FaFilm />;
-  const dailyNecessityIcon = <FaSprayCan />;
-  const clothesIcon = <FaTshirt />;
-  const beautyIcon = <FaCut />;
-  const medicalHealthIcon = <FaStethoscope />;
-  const educationIcon = <FaPencilAlt />;
-  const communicationIcon = <FaMobileAlt />;
-  const gatheringIcon = <FaBeer />;
-  const eventsIcon = <FaRegHandshake />;
-  const savingIcon = <FaPiggyBank />;
-  const electronicsIcon = <FaTv />;
-  const utilitiesIcon = <FaReceipt />;
-  const cardIcon = <FaCreditCard />;
-  const etcIcon = <FaEllipsisH />;
-  const salaryIcon = <FaMoneyBill />;
-  const moneyIcon = <FaMoneyCheck />;
-  const cashIcon = <FaMoneyCheckAlt  />;
-  const debitCardIcon = <FaCreditCard />;
-  const creditCardIcon = <FaCreditCard />;
-  
-  export {
-    foodIcon,
-    transportationIcon,
-    culturalIcon,
-    dailyNecessityIcon,
-    clothesIcon,
-    beautyIcon,
-    medicalHealthIcon,
-    educationIcon,
-    communicationIcon,
-    gatheringIcon,
-    eventsIcon,
-    savingIcon,
-    electronicsIcon,
-    utilitiesIcon,
-    cardIcon,
-    etcIcon,
-    salaryIcon,
-    moneyIcon,
-    cashIcon,
-    debitCardIcon,
-    creditCardIcon
-  }
