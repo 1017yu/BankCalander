0ca0715f1491e7a73cbd8a122368cfafa898e614
Kled <1017yu@daum.net>
2023-07-13 16:18:12 +0900

    fix: Header 지역적으로 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import Graph from '@/components/graph/Graph';
 import Layout from '@/components/common/Layout';
-import Header from '@/components/common/Header';
 import NotFound from '@/components/common/NotFound';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/components/Home/Home';
-
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />}></Route>
