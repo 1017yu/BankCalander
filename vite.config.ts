7383640c82dc9b94f942ee85bedc2e3fa8238f71
Kled <1017yu@daum.net>
2023-07-20 22:19:16 +0900

    docs: proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
-    port: 3000
+    proxy: {
+      '/api': {
+        target: 'https://chickenlecture.xyz/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    port: 3000,
   },
   envDir: '.',
-  }
-);
+});
